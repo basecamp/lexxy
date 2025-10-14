512768daeba8e442242169ab907c5a4950c47e81
Zoltan Hosszu <zoltan@37signals.com>
2025-10-14 10:58:38 +0200

    Removed trailing whitespaces

--- test/system/horizontal_divider_test.rb
+++ test/system/horizontal_divider_test.rb
@@ -8,15 +8,15 @@ class HorizontalDividerTest < ApplicationSystemTestCase
   test "insert horizontal divider via toolbar" do
     find_editor.send "Some text before"
     find_editor.send_key "Enter"
-    
+
     click_on "Insert a divider"
-    
+
     assert_selector "figure.horizontal-divider"
     assert_selector "figure.horizontal-divider hr"
-    
+
     find_editor.send_key "Enter"
     find_editor.send "Some text after"
-    
+
     assert_equal_html "<p>Some text before</p><hr><p>Some text after</p>", find_editor.value
   end
 
@@ -26,11 +26,11 @@ class HorizontalDividerTest < ApplicationSystemTestCase
     click_on "Insert a divider"
     find_editor.send_key "Enter"
     find_editor.send "Text after"
-    
+
     # Click on the divider to select it
     find("figure.horizontal-divider").click
     find_editor.send_key "Delete"
-    
+
     assert_no_selector "figure.horizontal-divider"
     assert_equal_html "<p>Text before</p><p>Text after</p>", find_editor.value
   end
@@ -41,10 +41,10 @@ class HorizontalDividerTest < ApplicationSystemTestCase
     click_on "Insert a divider"
     find_editor.send_key "Enter"
     find_editor.send "After divider"
-    
+
     assert_selector "figure.horizontal-divider"
     assert_selector "figure.horizontal-divider hr"
-    
+
     assert_equal_html "<p>Before divider</p><hr><p>After divider</p>", find_editor.value
   end
 end
