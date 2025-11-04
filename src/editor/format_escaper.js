faf5f2a7af47f5262951336667bed4675ba541b2
Jorge Manrubia <jorge@hey.com>
2025-11-04 10:22:38 +0100

    Extract methods

--- src/editor/format_escaper.js
+++ src/editor/format_escaper.js
@@ -22,12 +22,21 @@ export class FormatEscaper {
 
     const anchorNode = selection.anchor.getNode()
 
+    return this.#handleLists(event, anchorNode)
+      || this.#handleBlockquotes(event, anchorNode)
+  }
+
+  #handleLists(event, anchorNode) {
     if (this.#shouldEscapeFromEmptyListItem(anchorNode)) {
       event.preventDefault()
       this.#escapeFromList(anchorNode)
       return true
     }
 
+    return false
+  }
+
+  #handleBlockquotes(event, anchorNode) {
     if (this.#shouldEscapeFromEmptyParagraphInBlockquote(anchorNode)) {
       event.preventDefault()
       this.#escapeFromBlockquote(anchorNode)
