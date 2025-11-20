7774cdf14987cb03869229a628329ea91beba56a
Zoltan Hosszu <zoltan@37signals.com>
2025-11-20 10:23:28 +0100

    Extracted Color Dialog functions
    Close open dialogs on opening a new one

--- src/elements/color_dialog.js
+++ src/elements/color_dialog.js
@@ -1,11 +1,10 @@
 export class ColorDialog extends HTMLElement {
   connectedCallback() {
     this.dialog = this.querySelector("dialog")
-    //this.addEventListener("click", this.#handleColorButtonClick.bind(this))
     this.addEventListener("keydown", this.#handleKeyDown.bind(this))
     this.querySelector("[data-command='removeHighlight']").addEventListener("click", this.#handleRemoveHighlight.bind(this))
 
-    this.#buildButtons()
+    this.#setUpButtons()
   }
 
   show() {
@@ -16,37 +15,54 @@ export class ColorDialog extends HTMLElement {
     this.dialog.close()
   }
 
-  #buildButtons() {
-    const buttonGroups = this.querySelectorAll("[data-button-group]")
-    if (!buttonGroups) return
+  #handleKeyDown(event) {
+    if (event.key === "Escape") {
+      event.stopPropagation()
+      this.close()
+    }
+  }
 
-    buttonGroups.forEach(buttonGroup => {
-      const values = buttonGroup.dataset.values?.split(";") || []
-      values.forEach(value => {
-        const button = document.createElement("button")
-        button.classList.add("lexxy-color-button")
-        button.dataset.value = value
-        button.dataset.style = buttonGroup.dataset.buttonGroup
-        button.style.setProperty(buttonGroup.dataset.buttonGroup, value)
-        button.addEventListener("click", this.#handleColorButtonClick.bind(this))
-        buttonGroup.appendChild(button)
-      })
+  #setUpButtons() {
+    this.#buttonGroups.forEach(buttonGroup => {
+      this.#populateButtonGroup(buttonGroup)
     })
   }
 
-  #handleRemoveHighlight() {
-    this.#editor.dispatchCommand("removeHighlight")
-    this.close()
+  #populateButtonGroup(buttonGroup) {
+    const values = buttonGroup.dataset.values?.split(";") || []
+    const attribute = buttonGroup.dataset.buttonGroup
+    values.forEach((value, index) => {
+      buttonGroup.appendChild(this.#button(attribute, value, index))
+    })
+  }
+
+  #button(attribute, value, index) {
+    const button = document.createElement("button")
+    button.dataset.style = attribute
+    button.style.setProperty(attribute, value)
+    button.dataset.value = value
+    button.classList.add("lexxy-color-button")
+    button.name = attribute + "-" + index
+    button.addEventListener("click", this.#handleColorButtonClick.bind(this))
+    return button
   }
 
   #handleColorButtonClick(event) {
     event.preventDefault()
 
-    let color = {}
-
     const button = event.target.closest("button")
     if (!button) return
 
+    this.#toggleButtonState(button)
+
+    const attribute = button.dataset.style
+    const value = button.closest("[data-button-group]")?.querySelector("[aria-pressed='true']")?.dataset.value || ""
+
+    this.#editor.dispatchCommand("highlight", { [attribute]: value })
+    this.close()
+  }
+
+  #toggleButtonState(button) {
     const buttonGroup = button.closest("[data-button-group]")
     if (!buttonGroup) return
 
@@ -59,21 +75,17 @@ export class ColorDialog extends HTMLElement {
     } else {
       button.setAttribute("aria-pressed", "false")
     }
+  }
 
-    const attribute = button.dataset.style
-    const value = buttonGroup.querySelector("[aria-pressed='true']")?.dataset.value
+  #handleRemoveHighlight(event) {
+    event.preventDefault()
 
-    color = { [attribute]: value }
-
-    this.#editor.dispatchCommand("highlight", color)
+    this.#editor.dispatchCommand("removeHighlight")
     this.close()
   }
 
-  #handleKeyDown(event) {
-    if (event.key === "Escape") {
-      event.stopPropagation()
-      this.close()
-    }
+  get #buttonGroups() {
+    return this.querySelectorAll("[data-button-group]")
   }
 
   get #editor() {
