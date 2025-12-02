d160d4e97536fa3a5b16f6edaaa1682ccae19e99
Samuel Péchèr <samp@hey.com>
2025-12-02 13:14:34 +0000

    Select last descendant text node
    Prefer to set the cursor at the end of the TextNode if the last
    descendant is a TextNode, since seleting the end of the root node will
    cause a blank line!
    
    force focus of empty editor on p node

--- src/elements/editor.js
+++ src/elements/editor.js
@@ -131,7 +131,13 @@ export default class LexicalEditorElement extends HTMLElement {
   }
 
   focus() {
-    this.editor.focus()
+    this.editor.focus(() => this.#onFocus())
+  }
+
+  #onFocus() {
+    if (this.isEmpty) {
+      this.selection.placeCursorAtTheEnd()
+    }
   }
 
   get value() {
