370868b9ecee0fc9da18a6b4b2e7320aa9f11a80
Samuel Péchèr <samp@hey.com>
2025-12-12 16:39:57 +0000

    Filter and canonicalize styles on paste
    * Filter for PASTE_TAG during node generation
    * StyleCanonicalizer object caches canonical style for speed

--- src/helpers/format_helper.js
+++ src/helpers/format_helper.js
@@ -1,5 +1,6 @@
 import { $isRangeSelection } from "lexical"
 import { getStyleObjectFromCSS } from "@lexical/selection"
+import { createElement } from "./html_helper"
 
 export function isSelectionHighlighted(selection) {
   if (!$isRangeSelection(selection)) return false
@@ -14,4 +15,45 @@ export function isSelectionHighlighted(selection) {
 export function hasHighlightStyles(cssOrStyles) {
   const styles = typeof cssOrStyles === "string" ? getStyleObjectFromCSS(cssOrStyles) : cssOrStyles
   return !!(styles.color || styles["background-color"])
+}
+
+export class StyleCanonicalizer {
+  constructor(property, allowedValues= []) {
+    this._property = property
+    this._allowedValues = allowedValues
+    this._canonicalValues = this.#allowedValuesIdentityObject
+  }
+
+  getCanonicalAllowedValue(value) {
+    return this._canonicalValues[value] ||= this.#resolveCannonicalValue(value)
+  }
+
+  // Private
+
+  get #allowedValuesIdentityObject() {
+    return this._allowedValues.reduce((object, value) => ({ ...object, [value]: value }), {})
+  }
+
+  #resolveCannonicalValue(value) {
+    let index = this.#computedAllowedValues.indexOf(value)
+    index ||= this.#computedAllowedValues.indexOf(getComputedStyleForProperty(this._property, value))
+    return index === -1 ? null : this._allowedValues[index]
+  }
+
+  get #computedAllowedValues() {
+    return this._computedAllowedValues ||= this._allowedValues.map(
+      value => getComputedStyleForProperty(this._property, value)
+    )
+  }
+}
+
+function getComputedStyleForProperty(property, value) {
+  const style = `${property}: ${value};`
+
+  // the element has to be attached to the DOM have computed styles
+  const element = document.body.appendChild(createElement("span", { style: "display: none;" + style }))
+  const computedStyle = window.getComputedStyle(element).getPropertyValue(property)
+  element.remove()
+
+  return computedStyle
 }
