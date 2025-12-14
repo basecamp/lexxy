2d8d9ea227ca5e753f3a4cc4b253baa79957259f
Jorge Manrubia <jorge@hey.com>
2025-12-14 17:39:41 +0100

    Fix: don't sanitize CSS variables for mark nodes
    Or highlighitng colors won't work when rendering action text!

--- lib/lexxy/engine.rb
+++ lib/lexxy/engine.rb
@@ -40,6 +40,8 @@ module Lexxy
 
         default_allowed_attributes = Class.new.include(ActionText::ContentHelper).new.sanitizer_allowed_attributes
         ActionText::ContentHelper.allowed_attributes = default_allowed_attributes + %w[ controls poster data-language style ]
+
+        Loofah::HTML5::SafeList::ALLOWED_CSS_FUNCTIONS << "var" # Allow CSS variables
       end
     end
 
--- test/system/color_highlighter_test.rb
+++ test/system/color_highlighter_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class FormatTest < ApplicationSystemTestCase
+class ColorHighlighterTest < ApplicationSystemTestCase
   setup do
     visit edit_post_path(posts(:hello_world))
   end
@@ -24,5 +24,14 @@ class FormatTest < ApplicationSystemTestCase
     find_editor.send " again!"
 
     assert_equal_html "<p>Hello everyone<mark style=\"color: var(--highlight-1);\"> again!</mark></p>", find_editor.value
+  end
+
+  test "color highlighting is preserved after saving" do
+    find_editor.select "everyone"
+    apply_highlight_option "background-color", 1
+
+    click_on "Update Post"
+
+    assert_selector "mark[style='background-color:var(--highlight-bg-1);']", text: "everyone"
   end
 end
