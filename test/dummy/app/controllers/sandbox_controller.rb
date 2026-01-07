41a5ac5b2c8c62eb6c7d9370af167f287eee0ccf
Zoltan Hosszu <zoltan@37signals.com>
2026-01-07 12:16:38 +0100

    Add editor value templates to sandbox

--- test/dummy/app/controllers/sandbox_controller.rb
+++ test/dummy/app/controllers/sandbox_controller.rb
@@ -1,4 +1,9 @@
 class SandboxController < ApplicationController
   def show
   end
+
+  def load_content
+    partial_name = params[:partial]
+    render partial: partial_name, layout: false
+  end
 end
