73e9cd6b3b870128bc71e1a24bb541118de57c86
Zacharias Dyna Knudsen <z@chari.as>
2026-01-08 19:58:02 +0100

    Add `vitest` for JS unit testing
    Test for Configuration class and EditorConfiguration.
    Update GitHub Actions and local CI to run JS tests.

--- config/ci.rb
+++ config/ci.rb
@@ -4,8 +4,9 @@ CI.run do
   step "Style: Ruby", "bin/rubocop -f github"
   step "Style: JavaScript", "yarn lint"
 
-  step "Tests: prepare", "env RAILS_ENV=test bin/rails db:test:prepare"
-  step "Tests: run", "env RAILS_ENV=test bin/rails test:all -d"
+  step "Tests: JavaScript", "yarn test"
+  step "Tests: Rails (prepare)", "env RAILS_ENV=test bin/rails db:test:prepare"
+  step "Tests: Rails (run)", "env RAILS_ENV=test bin/rails test:all -d"
 
   unless success?
     failure "Signoff: CI failed.", "Fix the issues and try again."
