ccb7e7a3573d388dbd71fb04fe1750afcd3e0dd4
Zoltan Hosszu <zoltan@37signals.com>
2026-01-22 18:31:06 +0100

    Source rebuild

--- app/assets/javascript/lexxy.js
+++ app/assets/javascript/lexxy.js
@@ -6859,7 +6859,9 @@ class LexicalToolbarElement extends HTMLElement {
   }
 
   #toolbarIsOverflowing() {
-    return this.scrollWidth > this.clientWidth
+    // Safari can report inconsistent clientWidth values on more than 100% window zoom level,
+    // that was affecting the toolbar overflow calculation. We're adding +1 to get around this issue.
+    return (this.scrollWidth - this.#overflow.clientWidth) > this.clientWidth + 1
   }
 
   #refreshToolbarOverflow = () => {
