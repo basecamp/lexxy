e279434121a70fe4d322ae7bb7080702e7184ca6
Samuel Péchèr <samp@hey.com>
2026-03-16 10:04:16 +0000

    Detect arch in ci.rb

--- config/ci.rb
+++ config/ci.rb
@@ -1,4 +1,9 @@
 # Run using bin/ci
+require "etc"
+
+def arch_linux?
+  Etc.uname[:release].match?(/arch/)
+end
 
 CI.run do
   step "Style: Ruby", "bin/rubocop -f github"
@@ -8,7 +13,12 @@ CI.run do
   step "Tests: Rails (prepare)", "env RAILS_ENV=test bin/rails db:test:prepare"
   step "Tests: Rails (run)", "env RAILS_ENV=test bin/rails test:all -d"
 
-  step "Tests: Playwright (install)", "npx playwright install --with-deps chromium"
+  if arch_linux?
+    step "Tests: checking for chromium on arch", %(test -f "/usr/bin/chromium" && echo "local chromium found")
+    step "Tests: checking for firefox on arch",  %(test -f "/usr/bin/firefox" && echo "local firefox found")
+  else
+    step "Tests: Playwright (install)", "npx playwright install --with-deps chromium" unless ArchSystem
+  end
   step "Tests: Playwright (run)", "yarn test:browser:chromium"
 
   unless success?
