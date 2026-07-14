a04da8498237d4d129a4cd7cd673483d8c59a2ce
Jorge Manrubia <jorge@37signals.com>
2026-07-14 10:48:07 +0200

    Move selectAcrossDivider helper to the end of the test file

--- test/browser/tests/formatting/block_format_beside_decorator.test.js
+++ test/browser/tests/formatting/block_format_beside_decorator.test.js
@@ -1,31 +1,6 @@
 import { test } from "../../test_helper.js"
 import { assertEditorHtml } from "../../helpers/assertions.js"
 
-// Selects from the first text node to the last text node in the editor,
-// mirroring the production selection shapes that border a decorator: an
-// endpoint sitting flush against a block edge (offset 0 or the end of the
-// text) lands next to the <hr>, which is the case that used to throw.
-async function selectAcrossDivider(editor, { anchorAtEnd = false, focusAtEnd = false } = {}) {
-  await editor.click()
-  await editor.content.evaluate((el, { anchorAtEnd, focusAtEnd }) => {
-    const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT)
-    const textNodes = []
-    let node
-    while ((node = walker.nextNode())) textNodes.push(node)
-    const firstText = textNodes[0]
-    const lastText = textNodes[textNodes.length - 1]
-
-    const range = document.createRange()
-    range.setStart(firstText, anchorAtEnd ? firstText.nodeValue.length : 0)
-    range.setEnd(lastText, focusAtEnd ? lastText.nodeValue.length : 0)
-
-    const selection = window.getSelection()
-    selection.removeAllRanges()
-    selection.addRange(range)
-  }, { anchorAtEnd, focusAtEnd })
-  await editor.flush()
-}
-
 test.describe("Block formats next to decorator nodes", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/")
@@ -72,3 +47,28 @@ test.describe("Block formats next to decorator nodes", () => {
     await assertEditorHtml(editor, "<blockquote><p>alpha</p><hr><p>beta</p></blockquote>")
   })
 })
+
+// Selects from the first text node to the last text node in the editor,
+// mirroring the production selection shapes that border a decorator: an
+// endpoint sitting flush against a block edge (offset 0 or the end of the
+// text) lands next to the <hr>, which is the case that used to throw.
+async function selectAcrossDivider(editor, { anchorAtEnd = false, focusAtEnd = false } = {}) {
+  await editor.click()
+  await editor.content.evaluate((el, { anchorAtEnd, focusAtEnd }) => {
+    const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT)
+    const textNodes = []
+    let node
+    while ((node = walker.nextNode())) textNodes.push(node)
+    const firstText = textNodes[0]
+    const lastText = textNodes[textNodes.length - 1]
+
+    const range = document.createRange()
+    range.setStart(firstText, anchorAtEnd ? firstText.nodeValue.length : 0)
+    range.setEnd(lastText, focusAtEnd ? lastText.nodeValue.length : 0)
+
+    const selection = window.getSelection()
+    selection.removeAllRanges()
+    selection.addRange(range)
+  }, { anchorAtEnd, focusAtEnd })
+  await editor.flush()
+}
